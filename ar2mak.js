f272fe24565a29e82ddd0bc188f718ba405ebdf8
Kevin Frei <freik@fb.com>
2019-08-11 19:07:14 -0700

    Preparing for flow-remove-types

--- ar2mak.js
+++ ar2mak.js
@@ -4,7 +4,7 @@
 
 // Call main with boards.txt platform.txt programmers.txt
 
-const main = require('./main.js');
+const main = require('./src/main.js');
 
 main(process.argv[2], process.argv[3], process.argv[4]).then(a => { });
 
