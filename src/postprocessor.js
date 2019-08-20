14e8b69f418c5cd6b18cfce7c1ef5adbe3869f21
Kevin Frei <freik@fb.com>
2019-08-20 07:45:30 -0700

    Starting to work on the postprocessor

--- src/postprocessor.js
+++ src/postprocessor.js
@@ -19,18 +19,29 @@ const order = (
   defs: Array<Definition>,
   rules: Array<Recipe>
 ): { checks: Array<string>, defs: Array<Definition> } => {
-  return { checks: [], defs };
+  // Don't know if I'll need the rules or not
+
+  // First, let's identify all the mandatory user-defined symbols
+  const allDefs = new Set(defs.map(d => d.name));
+  const allDeps = new Set([].concat(...defs.map(d => d.dependsOn)));
+  // Remove allDefs from allDeps
+  const checks: Array<string> = [...allDeps].filter(x => !allDefs.has(x));
+
+  // Arbitrarily sort the definitions
+  const sorted = defs.sort((a:Definition) => a.name);
+
+  return { checks, defs: sorted };
 };
 
 const emitChecks = (checks: Array<string>) => {
   console.log('Emitting checks!');
 };
 
-const emitDefs = (defs:Array<Definition>) => {
+const emitDefs = (defs: Array<Definition>) => {
   console.log('Emitting definitions!');
 };
 
-const emitRules = (rules:Array<Recipe>) => {
+const emitRules = (rules: Array<Recipe>) => {
   console.log('Emitting rules!');
 };
 
