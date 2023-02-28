a5d6a20ed16f1943710fbab2c28ef299d1887282
Kevin Frei <kevinfrei@hotmail.com>
2023-02-27 20:12:20 -0800

    Got the libraries set up with some simple testing

--- src/__test__/bigdumbtest.ts
+++ src/__test__/bigdumbtest.ts
@@ -2,9 +2,12 @@ import { FileUtil } from '@freik/node-utils';
 import { promises } from 'fs';
 import main from '../main';
 
-const outputFiles = ['src/__test__/avr.txt', 'src/__test__/teensy.txt'];
-const avrOutput = outputFiles[0];
-const teensyOutput = outputFiles[1];
+const outputFiles = [
+  'src/__test__/avr.txt',
+  'src/__test__/teensy.txt',
+  'src/__test__/teensy-libs.txt',
+];
+const [avrOutput, teensyOutput, teensyLibsOutput] = outputFiles;
 
 async function deleteOutputs() {
   for (const file of outputFiles) {
@@ -34,19 +37,50 @@ beforeAll(deleteOutputs);
 afterAll(deleteOutputs);
 
 it('AVR basics', async () => {
-  expect('true').toEqual('true');
   expect(
     await main(`--out:${avrOutput}`, 'src/__test__/hardware/arduino/avr'),
   ).toBeFalsy();
-  expect(fileCompare(avrOutput, 'src/__test__/baseline.avr')).toBeTruthy();
+  expect(
+    await fileCompare(avrOutput, 'src/__test__/baseline.avr'),
+  ).toBeTruthy();
 });
 
 it('Teensy basics', async () => {
-  expect('true').toEqual('true');
   expect(
     await main(`--out:${teensyOutput}`, 'src/__test__/hardware/teensy/avr'),
   ).toBeFalsy();
   expect(
-    fileCompare(teensyOutput, 'src/__test__/baseline.teensy'),
+    await fileCompare(teensyOutput, 'src/__test__/baseline.teensy'),
+  ).toBeTruthy();
+});
+
+it('Teensy with some libs', async () => {
+  expect(
+    await main(
+      `--out:${teensyLibsOutput}`,
+      'src/__test__/hardware/teensy/avr',
+      'src/__test__/libraries/Adafruit_Circuit_Playground',
+      'src/__test__/libraries/Bridge',
+      'src/__test__/libraries/Esplora',
+      'src/__test__/libraries/Ethernet',
+      'src/__test__/libraries/Firmata',
+      'src/__test__/libraries/GSM',
+      'src/__test__/libraries/Keyboard',
+      'src/__test__/libraries/LiquidCrystal',
+      'src/__test__/libraries/Mouse',
+      'src/__test__/libraries/RobotIRremote',
+      'src/__test__/libraries/Robot_Control',
+      'src/__test__/libraries/Robot_Motor',
+      'src/__test__/libraries/SD',
+      'src/__test__/libraries/Servo',
+      'src/__test__/libraries/SpacebrewYun',
+      'src/__test__/libraries/Stepper',
+      'src/__test__/libraries/Temboo',
+      'src/__test__/libraries/TFT',
+      'src/__test__/libraries/WiFi',
+    ),
+  ).toBeFalsy();
+  expect(
+    await fileCompare(teensyLibsOutput, 'src/__test__/baseline.libs'),
   ).toBeTruthy();
 });
