c49d2be43a21dbb0c8a09911a8d49f89537c549c
Kevin Frei <kevinfrei@hotmail.com>
2023-03-16 23:01:51 -0700

    Duplicating existing stuff into the new SymbolTable

--- src/parser.ts
+++ src/parser.ts
@@ -3,8 +3,13 @@ import * as fs from 'node:fs';
 import * as rl from 'node:readline';
 
 import { Dump } from './dump';
-import { MakeSymbol } from './symbols';
-import type { DumbSymTbl, ParsedFile } from './types';
+import { MakeSymbol, MakeSymbolTable } from './symbols';
+import type {
+  DumbSymTbl,
+  ParsedFile,
+  ParsedSymbols,
+  SymbolTable,
+} from './types';
 
 function isComment(line: string): boolean {
   return line.trim().startsWith('#');
@@ -40,4 +45,38 @@ export async function ParseFile(filepath: string): Promise<ParsedFile> {
     }
   }
   return { scopedTable };
+}
+
+function parseVar(line: string, table: SymbolTable): boolean {
+  const t = line.trim();
+  const eq = t.indexOf('=');
+  /* istanbul ignore if */
+  if (eq < 1) {
+    return false;
+  }
+  const fullName = t.substring(0, eq);
+  const value = t.substring(eq + 1);
+  return !Type.isUndefined(table.add(fullName, value));
+}
+
+// Read in the text file, and spit out the parsed file
+export async function ParseSymbolTable(
+  filepath: string,
+): Promise<ParsedSymbols> {
+  const symTable = MakeSymbolTable();
+
+  const read = rl.createInterface({ input: fs.createReadStream(filepath) });
+  let num = 0;
+  for await (const line of read) {
+    num++;
+    if (isComment(line) || line.trim().length === 0) {
+      continue;
+    }
+    // Read the variables one by one
+    /* istanbul ignore if */
+    if (!parseVar(line, symTable)) {
+      Dump('err')(`Error ${num}: ${line}`);
+    }
+  }
+  return { symTable };
 }
